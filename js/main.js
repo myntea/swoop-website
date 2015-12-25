08f77c5d71b1ffdab55b8e54908d8609de9c9c25
Ruel Angelo Aballe <ruel.aballe@aestrea.com>
2015-12-26 04:13:23 +0800

    Fix anchor links and adds easing effect.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
 var swoop = swoop || {};
 swoop.init = function() {
     swoop.backToTop();
+    swoop.anchorEasing();
 };
 
 swoop.backToTop = function(){
-    $('.back-to-top').on('click', function (e) {
+    $('.back-to-top, .navbar-brand').on('click', function (e) {
         e.preventDefault();
         $('html,body').animate({
             scrollTop: 0
         }, 700);
     });
 };
+
+swoop.anchorEasing = function(){
+    $('a[href*=#]').on('click', function(event){
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    });
+}
 
 $(function(){
     swoop.init();
