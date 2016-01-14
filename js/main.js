cda9dc0fd743b4436c3396b73c42a72d02b2a92b
Ruel Angelo Aballe <ruel.aballe@aestrea.com>
2016-01-15 03:35:13 +0800

    Updated navbar and jquery script.

--- js/main.js
+++ js/main.js
@@ -15,14 +15,14 @@ swoop.backToTop = function(){
 
 swoop.anchorEasing = function(){
     $('#navbar').on('click', 'a[href*=#]', function(event){
+        var url = "#" + $.attr(this, 'href').substring($.attr(this, 'href').indexOf("#")+1);
         $('html, body').animate({
-            scrollTop: $( $.attr(this, 'href') ).offset().top
+            scrollTop: $( url ).offset().top
         }, 500);
         $('.navbar-toggle').click();
         return false;
     });
     $('#talk-to-swoop').on('click', function(event){
-        console.log('aw');
         $('html, body').animate({
             scrollTop: $( $.attr(this, 'href') ).offset().top
         }, 500);
